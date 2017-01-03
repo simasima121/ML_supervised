1d5a5243d374d6279080403a448185fd50e8ebce
Sim Virdi <simeransvirdi@gmail.com>
2017-01-03 11:32:29 +0000

    Docs: Created SVM classifier for email data, achieving 98% accuracy

---
+++ SVM/mini-project/svm_author_id.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+	
+import sys
+from time import time
+sys.path.append("tools/")
+from email_preprocess import preprocess
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+### Create the classifier
+clf = SVC(kernel="linear")
+
+### fit the classifier on the training features and labels
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0,3), "s"
+
+### use trained classifier to predict labels for the test features
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0,3), "s"
+
+### calculate and return the accuracy on the test data
+accuracy = accuracy_score(pred, labels_test)
+
+print accuracy
