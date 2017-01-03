cb1d9cd272ce536531a5c811f7e3be06d207f375
Sim Virdi <simeransvirdi@gmail.com>
2017-01-03 17:58:51 +0000

    Docs: Created Decision Tree classifier for terrain data

---
+++ Decision Trees/TerrainData/classifyDT.py
@@ -0,0 +1,11 @@
+def classify(features_train, labels_train):
+	"""return a trained decision tree classifer"""
+    
+    ### import decision tree from sklearn
+    from sklearn import tree
+
+    ### Create classifier and train it
+    clf = tree.DecisionTreeClassifier()
+    clf.fit(features_train, labels_train)
+
+    return clf
