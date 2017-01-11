abefb7c1f63ed256e342649d8f9da78e8e8f0596
Sim Virdi <simeransvirdi@gmail.com>
2017-01-11 14:19:48 +0000

    Feat: Plotting the regression for the salary and bonus data

--- Regressions/mini-project/finance_regression.py
+++ Regressions/mini-project/finance_regression.py
@@ -29,21 +29,21 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
-
-
+test_color = "r"
 
 ### Your regression goes here!
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn import linear_model
 
+# creating and training the classifier
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
 
-
-
-
-
-
+# finding the slope and y intercept
+print reg.coef_[0]
+print reg.intercept_
 
 ### draw the scatterplot, with color-coded training and testing points
 import matplotlib.pyplot as plt
