9974a169fb6e23d09376cf3aad9dec61f6918ce8
Sim Virdi <simeransvirdi@gmail.com>
2017-01-11 10:34:14 +0000

    Docs: Did linear regression on age and net worth data

---
+++ Regressions/age_and_net_worth/studentRegression.py
@@ -0,0 +1,9 @@
+def studentReg(ages_train, net_worths_train):
+    ### import the sklearn regression module, create, and train your regression
+    from sklearn import linear_model
+    
+    ### Create and training regression 
+    reg = linear_model.LinearRegression()
+    reg.fit(ages_train, net_worths_train)
+    
+    return reg
