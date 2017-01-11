4459a7b29016da4efb6326a14665279fb03e26e4
Sim Virdi <simeransvirdi@gmail.com>
2017-01-11 14:42:52 +0000

    Feat: Creating second regression line on training data (without outlier)

--- Regressions/mini-project/finance_regression.py
+++ Regressions/mini-project/finance_regression.py
@@ -21,7 +21,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "long_term_incentive"]
+features_list = ["bonus", "salary"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -67,6 +67,12 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+# Creating second regression line on training data (without outlier)
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b") 
+print reg.coef_[0]
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
