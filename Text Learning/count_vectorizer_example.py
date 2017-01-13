dd3ff09d971f16633696dccd68c81d70758aba0f
Sim Virdi <simeransvirdi@gmail.com>
2017-01-13 15:45:36 +0000

    Fix: Updating comments for vectoriser

--- Text Learning/count_vectorizer_example.py
+++ Text Learning/count_vectorizer_example.py
@@ -14,5 +14,10 @@ email_list = [str1, str2, str3]
 ## fitting and transforming data for the vectoriser
 bag_of_words = vectoriser.fit_transform(email_list)
 
-## You get a bag of tuples and integer values
+## You get a bag of tuples and integer values, zero-indexed
+## Way to intepret is for (1, 7) 1, document 1/word number 7 - occurs 1 time
+## Words are all ordered in bag_of_words, thus for (2,15) it means the 15th word in bag_of_words, not in str3
 print bag_of_words
+
+## Checking if (1, 6) 3 is actually the word great - it is!
+print vectoriser.vocabulary_.get("great")
