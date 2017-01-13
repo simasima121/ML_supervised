e8e2dedd3b4396215c5894f14c5b351ab6fecb85
Sim Virdi <simeransvirdi@gmail.com>
2017-01-13 21:29:29 +0000

    Feat: Cleaned away signature words

--- Text Learning/mini-project/text_learning/vectorize_text.py
+++ Text Learning/mini-project/text_learning/vectorize_text.py
@@ -44,21 +44,28 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
         temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
-            print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            email_text = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+            replace_words = ["sara", "shackleton", "chris", "germani"]
+            
+            for w in replace_words:
+                email_text = email_text.replace(w, "")
 
             ### append the text to word_data
+            word_data.append(email_text)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if from_person == from_sara:
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
             email.close()
-
+print word_data[152]
 print "emails processed"
 from_sara.close()
 from_chris.close()
