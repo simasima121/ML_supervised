fd49d77ed27eb5b73db96b0b42a40439b98a6dab
Sim Virdi <simeransvirdi@gmail.com>
2017-01-19 17:33:31 +0000

    Docs: Creating Evaluation folder

---
+++ Evaluation/mini-project/evaluation/evaluate_poi_identifier.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+
+"""
+    Starter code for the evaluation mini-project.
+    Start by copying your trained/tested POI identifier from
+    that which you built in the validation mini-project.
+
+    This is the second step toward building your POI identifier!
+
+    Start by loading/formatting the data...
+"""
+
+import pickle
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+
+### add more features to features_list!
+features_list = ["poi", "salary"]
+
+data = featureFormat(data_dict, features_list)
+labels, features = targetFeatureSplit(data)
+
+## Splitting data into training and test sets
+from sklearn.cross_validation import train_test_split
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+## Creating decision tree classifier 
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred, labels_test)
+
+
+
