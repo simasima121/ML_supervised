bce17ecacd46c851c75f765af16e789bed3f1d3e
simasima121 <sim@Sims-MBP.home>
2017-02-20 18:38:23 +0000

    Feat: Used features with Adaboost DT

--- Assignment/final_project/poi_id.py
+++ Assignment/final_project/poi_id.py
@@ -1,7 +1,8 @@
-#!/usr/bin/python
+#!/Users/sim/anaconda3/envs/enron/bin/python
 
 import sys
 import pickle
+import matplotlib.pyplot
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
@@ -10,13 +11,17 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi','salary', 'exercised_stock_options', 'bonus', 'from_messages', 'from_poi_to_this_person', 'from_this_person_to_poi'] 
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
+### Feature Scaling
+### Scale the from_poi_to_this_person, from_messages and from_this_person_to_poi
+
 ### Task 2: Remove outliers
+
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
@@ -25,15 +30,38 @@ my_dataset = data_dict
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
+# Example starting point. Try investigating other evaluation techniques!
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
 ### Note that if you want to do PCA or other multi-stage operations,
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
-# Provided to give you a starting point. Try a variety of classifiers.
+# Using Naive Bayes Classifier
 from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+from sklearn.metrics import recall_score, precision_score, accuracy_score # measures of performance
+
+clf1 = GaussianNB()
+clf1.fit(features_train, labels_train)
+pred1 = clf1.predict(features_test)
+
+print 'recall ', recall_score(labels_test, pred1)
+print 'precision ', precision_score(labels_test, pred1)
+print 'accuracy_score ', accuracy_score(labels_test, pred1)
+
+# Using AdaBoost DT Classifer
+from sklearn.ensemble import AdaBoostClassifier
+clf2 = AdaBoostClassifier(n_estimators=100)
+clf2.fit(features_train, labels_train)
+pred2 = clf2.predict(features_test)
+
+print 'recall ', recall_score(labels_test, pred2)
+print 'precision ', precision_score(labels_test, pred2)
+print 'accuracy_score ', accuracy_score(labels_test, pred2)
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -42,14 +70,10 @@ clf = GaussianNB()
 ### stratified shuffle split cross validation. For more info: 
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
+
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
-
-dump_classifier_and_data(clf, my_dataset, features_list)
+#dump_classifier_and_data(clf, my_dataset, features_list)
