0b813b1addce8f93faceadbc4547b7d810dd63ab
simasima121 <sim@Sims-MBP.home>
2017-02-21 17:03:38 +0000

    Feat: Used new features and pipeline for PCA with Adaboost DT

--- Assignment/final_project/poi_id.py
+++ Assignment/final_project/poi_id.py
@@ -14,30 +14,33 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi', "bonus","deferral_payments", "deferred_income", "director_fees",
-             "exercised_stock_options", "expenses", "from_poi_to_this_person", 
-             "from_this_person_to_poi", "loan_advances", "long_term_incentive", "other",
-             "restricted_stock", "restricted_stock_deferred", "salary", 
-             "shared_receipt_with_poi", "total_payments", "total_stock_value"]
+## Loan advances removed as few entries, deferral_payments removed as gave worse fit
+## Director Fees removed as only Non-POI entries, Restricted stock deferred removed as not enough entries for POI
+features_list = ['poi', "bonus", "salary", "expenses", "deferred_income", "exercised_stock_options",  "long_term_incentive", "other", "restricted_stock", "restricted_stock_deferred", "total_payments", "total_stock_value", "from_poi_to_this_person", "from_this_person_to_poi"]
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
-### To spot outliers, I used this link http://bl.ocks.org/dmenin/raw/d12a22521ad32cacc906/
-### to identify potential issues.
+
+### To spot outliers, I used link http://bl.ocks.org/dmenin/raw/d12a22521ad32cacc906/
+### to identify potential issues and created my own boxplots with the following
+### code.
+def draw_boxplots(entry):
+	data_dict.pop('TOTAL', 0)
+	dat_dict_pd = [{k: data_dict[p][k]if data_dict[p][k] != 'NaN' else None for k in data_dict[p].keys()}for p in data_dict.keys()]
+	dat_pd = pd.DataFrame(dat_dict_pd)
+
+	# name your plot
+	ax = dat_pd.boxplot(entry, by = 'poi')
+	# save your plot to the folder that your code is in:
+	fig = ax.get_figure()
+	fig.savefig('%s.png' % entry)
 
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
-
-## Feature Scaling
-## Scale the money orientated features
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
-
-
 
 ### Extract features and labels from dataset for local testing.
 ### Data is a numpy array
@@ -55,6 +58,23 @@ features_train, features_test, labels_train, labels_test = \
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
+# Using AdaBoost DT with PCA classifier
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.decomposition import PCA
+from sklearn.metrics import recall_score, precision_score, accuracy_score # measures of performance
+
+## creating a pipeline and fitting it
+estimators = [('reduce_dim', PCA()), ('clf', AdaBoostClassifier(n_estimators=100))]
+pipe = Pipeline(estimators)
+clf = pipe
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+print 'recall ', recall_score(labels_test, pred)
+print 'precision ', precision_score(labels_test, pred)
+print 'accuracy_score ', accuracy_score(labels_test, pred)
+
 # Using Naive Bayes Classifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import recall_score, precision_score, accuracy_score # measures of performance
@@ -67,15 +87,6 @@ print 'recall ', recall_score(labels_test, pred1)
 print 'precision ', precision_score(labels_test, pred1)
 print 'accuracy_score ', accuracy_score(labels_test, pred1)
 
-# Using AdaBoost DT Classifer
-from sklearn.ensemble import AdaBoostClassifier
-clf2 = AdaBoostClassifier(n_estimators=100)
-clf2.fit(features_train, labels_train)
-pred2 = clf2.predict(features_test)
-
-print 'recall ', recall_score(labels_test, pred2)
-print 'precision ', precision_score(labels_test, pred2)
-print 'accuracy_score ', accuracy_score(labels_test, pred2)
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -90,4 +101,4 @@ print 'accuracy_score ', accuracy_score(labels_test, pred2)
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
-#dump_classifier_and_data(clf, my_dataset, features_list)
+dump_classifier_and_data(clf, my_dataset, features_list)
